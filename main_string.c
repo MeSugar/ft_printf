e6c755027e6b23de94f257f0853ebe16754ee86e
Eugene Vlasov <vlasov3454@yandex.ru>
2021-01-12 00:26:07 +0300

    string treat fixed

--- main_string.c
+++ main_string.c
@@ -7,13 +7,13 @@ int main()
     int b;
     char c[] = "abc";
     char d[] = "opq";
-    a = printf("1 o: %0.2s\n2 o: %-5s\n", c, d);
+    a = printf("1 o: %6.4s\n2 o: %-5s\n", c, d);
     printf("o int: %d\n\n", a);
     //printf("2:%-2c\n", c);
     //printf("3: %.3c\n", c);
     //printf("4:%3c\n", c);
 
-    b = ft_printf("1 m: %.2s\n2 m: %-5s\n", c, d);
+    b = ft_printf("1 m: %6.4s\n2 m: %-5s\n", c, d);
     printf("m int: %d\n\n", b);
 
     return (0);
